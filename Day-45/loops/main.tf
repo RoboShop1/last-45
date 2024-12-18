c47ccb3cdb50a2b4366a2851d6af2e7670cda5d1
chaithanya <chaithanya1812@gmail.com>
2024-12-19 01:28:27 +0530

    day-45-tf

---
+++ Day-45/loops/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "instances" {
+  default = {
+    db = {}
+    cart = {}
+    frontend = {}
+  }
+}
+resource "aws_instance" "main" {
+  for_each = var.instances
+  ami = "ami-01816d07b1128cd2d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = lookup(var.instances,each.key,null)
+  }
+}
