cb30de6181415cb1f8958057145c6106e25490c1
chaithanya <chaithanya1812@gmail.com>
2024-12-19 19:59:24 +0530

    day-45-tf

--- Day-44/python/1.py
+++ Day-44/python/1.py
@@ -1,1 +1,18 @@
+import requests
+
+# Define the API endpoint
+url = "https://jsonplaceholder.typicode.com/posts"
+
+# Make a GET request
+response = requests.get(url)
+
+print(response)
+
+# Check if the request was successful
+# if response.status_code == 200:
+#     # Parse the JSON response
+#     data = response.json()
+#     print(data)
+# else:
+#     print(f"Failed to retrieve data: {response.status_code}")
 
