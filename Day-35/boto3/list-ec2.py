b9827e2070549289a2f06264d6a37d113c347981
chaithanya <chaithanya1812@gmail.com>
2024-12-24 10:54:47 +0530

    day-45-tf

--- Day-35/boto3/list-ec2.py
+++ Day-35/boto3/list-ec2.py
@@ -4,7 +4,6 @@ ec2_client = boto3.client(service_name="ec2",region_name="us-east-1")
 
 response = ec2_client.describe_instances()
 
-print(response["Reservations"])
 
 
 for i in response["Reservations"]:
