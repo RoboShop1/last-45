2641732c33ff9ab716234e9bef709862bbe21510
chaithanya <chaithanya1812@gmail.com>
2024-12-24 11:10:33 +0530

    day-45-tf

---
+++ Day-35/boto3/create-ec2.py
@@ -0,0 +1,27 @@
+import boto3
+
+
+ec2_client = boto3.client(service_name="ec2",region_name="us-east-1")
+
+
+response = ec2_client.run_instances(
+    ImageId='ami-01816d07b1128cd2d',
+    InstanceType='t2.micro',
+    MaxCount=2,
+    MinCount=2,
+
+    TagSpecifications=[{
+        'ResourceType' = 'instance',
+        'Tags': [{
+            'Key': 'Name'
+            'Value': 'boto3'
+        }]
+    }]
+    )
+
+
+print(response)
+
+
+
+
