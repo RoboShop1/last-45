fdc498ed27a970de59fb754513893b4bf03dca08
chaithanya <chaithanya1812@gmail.com>
2025-01-06 23:17:16 +0530

    day-45-tf

---
+++ Day-31/terraform/main.tf
@@ -0,0 +1,17 @@
+module "vpc-dev" {
+
+  source = "./modules/vpc"
+
+  vpc_cidr_block = ["10.0.0.0/16"]
+  env            = "dev"
+  public_subnets = ["10.0.1.0/24","10.0.2.0/24"]
+  web_subnets    = ["10.0.3.0/24","10.0.4.0/24"]
+  app_subnets    = ["10.0.5.0/24","10.0.6.0/24"]
+  db_subnets     = ["10.0.7.0/24","10.0.8.0/24"]
+
+  public_azs    = ["us-east-1a","us-east-1b"]
+  web_azs       = ["us-east-1a","us-east-1b"]
+  app_azs       = ["us-east-1a","us-east-1b"]
+  db_azs        = ["us-east-1a","us-east-1b"]
+
+}
