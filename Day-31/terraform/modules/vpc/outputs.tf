1f434a249d10847cfe66f1ec52bbd048aff997db
chaithanya <chaithanya1812@gmail.com>
2025-01-10 14:06:37 +0530

    day-45-tf

--- Day-31/terraform/modules/vpc/outputs.tf
+++ Day-31/terraform/modules/vpc/outputs.tf
@@ -1,3 +1,22 @@
 output "vpc_details" {
   value = aws_vpc.main
 }
+
+output "public_subnets" {
+  value = aws_subnet.public-subnets
+}
+
+
+
+output "web_subnets" {
+  value = aws_subnet.web-subnets
+}
+
+output "app_subnets" {
+  value = aws_subnet.app-subnets
+}
+
+
+output "db_subnets" {
+  value = aws_subnet.db-subnets
+}
