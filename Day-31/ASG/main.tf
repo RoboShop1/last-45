a5923416d50e7fa4a5b0fa5f6b182a3f830ef706
chaithanya <chaithanya1812@gmail.com>
2025-01-12 08:23:19 +0530

    day-45-tf

--- Day-31/ASG/main.tf
+++ Day-31/ASG/main.tf
@@ -75,8 +75,8 @@ resource "aws_launch_template" "foo" {
   user_data = filebase64("${path.module}/example.sh")
 }
 
-# resource "aws_instance" "main" {
-#   launch_template {
-#     id = aws_launch_template.foo.id
-#   }
-# }
+resource "aws_instance" "main" {
+  launch_template {
+    id = aws_launch_template.foo.id
+  }
+}
