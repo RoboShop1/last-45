a2e8a3e26e863e397150718d2d767d007d5d7f50
chaithanya <chaithanya1812@gmail.com>
2025-01-12 10:11:01 +0530

    day-45-tf

--- Day-31/ASG/auto-sg.tf
+++ Day-31/ASG/auto-sg.tf
@@ -1,15 +1,15 @@
-# resource "aws_autoscaling_group" "example" {
-#   availability_zones = ["us-east-1a","us-east-1b"]
-#   desired_capacity   = 3
-#   max_size           = 6
-#   min_size           = 1
-#
-#   mixed_instances_policy {
-#     launch_template {
-#       launch_template_specification {
-#         launch_template_id = aws_launch_template.foo.id
-#         version = "$Latest"
-#       }
-#     }
-#   }
-# }
+resource "aws_autoscaling_group" "example" {
+  availability_zones = ["us-east-1a","us-east-1b"]
+  desired_capacity   = 3
+  max_size           = 6
+  min_size           = 1
+
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.foo.id
+        version = "$Latest"
+      }
+    }
+  }
+}
