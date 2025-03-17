d75c58e92902c24fdef9c596407d896ec2bfbe8c
chaithanya <chaithanya1812@gmail.com>
2025-03-17 10:28:18 +0530

    ss

--- python-jwt/file.py
+++ python-jwt/file.py
@@ -1,6 +1,2 @@
 with open('/tmp/example.txt', 'r') as file:
-    line1 = file.readline()
-    print(line1.strip())  # Reads the first line
-
-    line2 = file.readline()
-    print(line2.strip())
+    print(file.read())
