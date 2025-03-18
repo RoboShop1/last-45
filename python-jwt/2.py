636459e507fdf5211589a0651177b272400a69e1
chaithanya <chaithanya1812@gmail.com>
2025-03-18 14:02:14 +0530

    ss

--- python-jwt/2.py
+++ python-jwt/2.py
@@ -1,12 +1,4 @@
-members = ["one", "two","three"]
-
-if "one" in members:
-    print("yes - present")
-
-val = {
-  "one": 1,
-  "two": 2,
-}
-
-if 2 in val.values():
-        print("yes - present")
+try:
+    print("name",+,"chaitu")
+except:
+    print("lllll")
