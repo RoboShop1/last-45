362cef7677c8800dc4cc3e598107d07892791b66
chaithanya <chaithanya1812@gmail.com>
2025-03-18 13:46:43 +0530

    ss

--- python-jwt/python.py
+++ python-jwt/python.py
@@ -14,19 +14,23 @@ payload_json = json.dumps(payload, separators=(",", ":"))
 encoded_payload = base64.urlsafe_b64encode(payload_json.encode()).decode().rstrip("=")
 print("Encoded Payload:", encoded_payload)
 
+token_actual = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+print(token_actual)
+decoded_payload = jwt.decode(token_actual, SECRET_KEY, algorithms=["HS256"])
+print(decoded_payload)
+
+
+print("=" *10)
+payload1 = {
+    "user_id": 123,
+    "role": "admin2"
+}
+payload_json = json.dumps(payload1, separators=(",", ":"))
+encoded_payload = base64.urlsafe_b64encode(payload_json.encode()).decode().rstrip("=")
+print("Encoded Payload:", encoded_payload)
 print("=" *10)
 
-token_actual = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
-print(token_actual)
-
-
-
-
-decoded_payload = jwt.decode(token_actual, SECRET_KEY, algorithms=["HS256"])
-
-
-print(decoded_payload)
 
 
 
