78990ce945afadffb27efcc04aca8fbdaf914a0d
chaithanya <chaithanya1812@gmail.com>
2025-03-18 13:44:24 +0530

    ss

--- python-jwt/python.py
+++ python-jwt/python.py
@@ -13,7 +13,7 @@ payload_json = json.dumps(payload, separators=(",", ":"))
 encoded_payload = base64.urlsafe_b64encode(payload_json.encode()).decode().rstrip("=")
 print("Encoded Payload:", encoded_payload)
 
-print("=",*10)
+print("=" *10)
 
 token_actual = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
