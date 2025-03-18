213dbeb25db1faafdaef1dd66a7b18917b7c18f7
chaithanya <chaithanya1812@gmail.com>
2025-03-18 13:38:02 +0530

    ss

---
+++ python-jwt/convert.py
@@ -0,0 +1,16 @@
+import json
+import base64
+
+# Payload
+payload = {
+    "user_id": 123,
+    "role": "admin1"
+}
+
+# Convert to JSON string
+payload_json = json.dumps(payload, separators=(",", ":"))
+
+# Encode to Base64 URL-safe format (without padding)
+encoded_payload = base64.urlsafe_b64encode(payload_json.encode()).decode().rstrip("=")
+
+print("Encoded Payload:", encoded_payload)
