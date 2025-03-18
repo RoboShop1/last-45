a2afabc868dcd13a82f0b311e7452f59f3a3fe87
chaithanya <chaithanya1812@gmail.com>
2025-03-18 20:54:36 +0530

    ss

--- python-jwt/public-private-jwt/private-jwt.py
+++ python-jwt/public-private-jwt/private-jwt.py
@@ -1,3 +1,13 @@
+''''
+ssh-keygen -t rsa -b 4096 -m PEM -f jwt_rsa_key
+
+
+## ssh-keygen creates public keys in OpenSSH format by default, but PyJWT expects the PEM format.
+
+ssh-keygen -e -m PEM -f jwt_rsa_key.pub > jwt_rsa_key_pub.pem
+
+'''
+
 import jwt
 import json
 import base64
@@ -14,3 +24,13 @@ with open("/root/good", "r") as key_file:
 token = jwt.encode(payload, private_key, algorithm="RS256").decode("utf-8")
 
 print(token)
+
+
+print("#" *10)
+
+with open("/root/good.pub", "r") as key_file:
+    public_key = key_file.read()
+
+# Decode the JWT (verify with public key)
+decoded = jwt.decode(token, public_key, algorithms=["RS256"])
+print("Decoded JWT:", decoded)
